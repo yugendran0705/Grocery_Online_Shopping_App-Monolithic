a8da9027ba7052cb86ec69a15025f2118e089da0
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-17 22:01:17 +0530

    Create Customer.js

---
+++ src/database/models/Customer.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const customerSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    address: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Address'
+    }],
+    cart: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Product'
+    }],
+    orders: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Order'
+    }],
+    wishlist: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Product'
+    }]
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
