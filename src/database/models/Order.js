99b19d624d8e7fa2b24dd7ec2645bd5dd3eb71e2
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-19 23:22:42 +0530

    Update Order.js

--- src/database/models/Order.js
+++ src/database/models/Order.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    customer: {
-        type: Schema.Types.ObjectId,
-        ref: 'Customer'
+    order_id: {
+        type: String,
+        required: true
     },
-    products: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Product'
-    }],
-    total: {
+    customerId: {
+        type: String,
+        required: true
+    },
+    amount: {
         type: Number,
         required: true
     },
@@ -19,10 +19,22 @@ const orderSchema = new Schema({
         type: String,
         required: true
     },
-    date: {
-        type: Date,
+    tnxId: {
+        type: String,
         required: true
-    }
+    },
+    items: [
+        {
+            product: {
+                type: Schema.Types.ObjectId, ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
 });
 
 module.exports = mongoose.model('Order', orderSchema);
