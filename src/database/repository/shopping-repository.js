063427499dbfc04cc7069a002e5c4c6b290241e7
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-19 23:22:59 +0530

    Update shopping-repository.js

---
+++ src/database/repository/shopping-repository.js
@@ -0,0 +1,51 @@
+const DefinedError = require('../utils/DefinedError');
+const { ProductModel, CustomerModel, OrderModel } = require('../models');
+const { v4: uuidv4 } = require('uuid');
+
+class ShoppingRepository {
+    orders = async ({ _id }) => {
+        try {
+            const orders = await OrderModel.find({ customer: _id });
+            return orders;
+        }
+        catch (err) {
+            throw new DefinedError("Error finding orders", 500)
+        }
+    }
+
+    createNewOrder = async ({ customer_id, TnxId }) => {
+        try {
+            const profile = await CustomerModel.findById(customer_id).populate('cart.product');
+            if (!profile) {
+                throw new DefinedError("Customer not found", 404)
+            }
+            else {
+                var amount = 0;
+                profile.cart.forEach(element => {
+                    amount = amount + element.product.price;
+                }
+                );
+                const order = new OrderModel({
+                    order_id: uuidv4(),
+                    customer: customer_id,
+                    amount: amount,
+                    status: "Pending",
+                    tnxId: TnxId,
+                    items: profile.cart
+                });
+
+                profile.cart = [];
+                order.populate('items.product');
+                const order_save = await order.save();
+                profile.orders.push(order_save);
+                await profile.save();
+                return order_save;
+            }
+        }
+        catch (err) {
+            throw new DefinedError("Error creating order", 500)
+        }
+    }
+}
+
+module.exports = ShoppingRepository
