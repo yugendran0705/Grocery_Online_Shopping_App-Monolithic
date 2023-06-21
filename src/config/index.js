b51a58c0e9bd4cd4fa1203d2e237e6741b7d31d2
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-21 14:13:53 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,8 @@
 const dotEnv = require('dotenv');
 
-if (process.env.NODE_ENV !== 'production') {
-    const configfile = process.env.NODE_ENV === 'dev' ? './.env.dev' : './.env';
-    dotEnv.config({ path: configfile });
-}
-
+dotEnv.config();
 module.exports = {
     port: process.env.PORT,
-    dbUrl: process.env.DB_URL,
+    dbUrl: process.env.MONGO_URI,
     jwtSecret: process.env.JWT_SECRET,
 }
